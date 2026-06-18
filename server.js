bbd42f41493b500d9aae0bf32f451bc50121a43c
uttkarsh tiwari <uttkarshtiwari299@gmail.com>
2026-06-18 14:39:46 +0530

    Add CORS middleware

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express=require('express')
 const colors=require('colors')
 const morgan=require('morgan')
+const cors=require('cors')
 const dotenv=require('dotenv');
 const connectDB = require('./config/db');
 const path=require('path')
 dotenv.config();
 connectDB();
 const app=express()
+app.use(cors()) 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use("/api/v1/user",require("./routes/userRoutes"));
